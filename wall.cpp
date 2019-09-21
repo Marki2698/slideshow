83bd0c4c0f5d15a697c1f7600fcdb40de8168113
Marki <STIG3269@gmail.com>
2019-09-21 18:17:01 +0300

    added auto start option but it does not work

--- wall.cpp
+++ wall.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <Windows.h>
+#include <fileapi.h>
 #include <iostream>
 #include <chrono>
 #include <thread>
@@ -12,7 +13,13 @@
 #include <iterator>
 #include <regex>
 
+const bool GET_KEY_FOR_REMOVE = true;
+const bool CREATE_AND_GET_MODE = true;
+const bool GET_MODE_ONLY = false;
 const std::string FILE_OF_PATHES = "pathes.txt";
+const wchar_t* APP_NAME = L"WallpaperTracker";
+const wchar_t* DELETE_APP_REG_PATH = L"Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Wallpapers\\";
+const wchar_t* COMMAND_PATH = L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
 
 template <bool vectorOnly = true> //maybe remove template
 void addPathesToVectorOrPathFile(std::vector<std::string>& pathVec, std::string pathOfDir) {
@@ -57,6 +64,85 @@ void getPathesFromFile(std::vector<std::string>& pathesVec) {
 	}
 }
 
+std::wstring getFullPathOfFile() { // OK
+	wchar_t buffer[255];
+	DWORD bufferLength = 255; 
+	GetFullPathName(L"wall.exe", bufferLength, buffer, NULL);
+	std::wstring wstr(buffer);
+	return wstr;
+}
+
+template<bool fullMode = true>
+HKEY createAndGetKey() {
+	HKEY hKey;
+
+	//const wchar_t* path = COMMAND_PATH;
+	LONG result = -1;
+	result = RegOpenKeyEx(HKEY_CURRENT_USER, COMMAND_PATH, 0, KEY_READ, &hKey);
+	if (result != ERROR_SUCCESS) {
+		if (result == ERROR_FILE_NOT_FOUND) {
+			std::cout << "no such key" << std::endl;
+			if (fullMode) {
+				result = RegCreateKey(HKEY_CURRENT_USER, COMMAND_PATH, &hKey);
+				if (result == ERROR_SUCCESS) {
+					std::cout << "xcbvb" << std::endl;
+				}
+			}
+		}
+	}
+	else {
+		std::cout << "key opened" << std::endl;
+	}
+
+	return hKey;
+}
+
+
+//LONG getAutoStartStatus() {
+//	std::wstring absPath = getFullPathOfFile();
+//	HKEY hKey = createAndGetKey();
+//
+//	LONG lnRes = 0;
+//	lnRes = RegSetValueEx(hKey, APP_NAME, 0, REG_SZ, (BYTE*)absPath.c_str(), (absPath.size() + 1) / sizeof(wchar_t));
+//	RegCloseKey(hKey);
+//	if (hKey) {
+//		lnRes = RegSetValueEx(hKey, APP_NAME, 0, REG_SZ, (BYTE*)absPath.c_str(), (absPath.size() + 1) / sizeof(wchar_t));
+//		RegCloseKey(hKey);
+//	}
+//	else {
+//		std::cout << "something went wrong" << std::endl;
+//	}
+//
+//	return lnRes;
+//}
+
+void setAutoStart() {
+	std::wstring absPath = getFullPathOfFile();
+	HKEY hKey = createAndGetKey();
+
+	LONG lnRes = 0;
+
+	if (hKey) {
+		lnRes = RegSetValueEx(hKey, APP_NAME, 0, REG_SZ, (BYTE*)absPath.c_str(), (absPath.size() + 1) / sizeof(wchar_t));
+		if (lnRes == ERROR_SUCCESS) std::cout << "added to startup" << std::endl;
+		RegCloseKey(hKey);
+	}
+	else {
+		std::cout << "something went wrong" << std::endl;
+	}
+}
+
+void removeAutoStart() { // change it 
+	HKEY hKey = createAndGetKey<GET_MODE_ONLY>();
+	LONG res = RegDeleteValue(hKey, APP_NAME);
+	if (res == ERROR_SUCCESS) {
+		std::cout << "removed" << std::endl;
+	}
+	else {
+		std::cout << "something went wrong" << std::endl;
+	}
+}
+
 int main()
 {
 	std::vector<std::string> pathes = {}; // OK
@@ -64,7 +150,7 @@ int main()
 
 	std::string pathDir = "";
 	if (pathes.empty()) {
-		std::cout << " do you want to add dir with images ? (yes, no)";
+		std::cout << " do you want to add dir with images ? (yes, no) ";
 		std::string answer = "";
 		std::cin >> answer;
 		std::cout << std::endl;
@@ -79,7 +165,36 @@ int main()
 			exit(0);
 		}
 	}
-	
+
+	HKEY hKey = createAndGetKey<GET_MODE_ONLY>();
+
+	std::string answer = "";
+
+	if (hKey) {
+		std::cout << "Seems you use auto start up option do you want to turn off it ? (yes, no) ";
+		std::cin >> answer;
+		std::cout << answer << std::endl;
+		if (answer == "yes") {
+			removeAutoStart();
+		}
+		else {
+			std::cout << "enjoy app" << std::endl;
+		}
+		answer = "";
+	}
+	else {
+		std::cout << "Do you want to add utility to auto startup? (yes, no) ";
+		std::cin >> answer;
+		if (answer == "yes") {
+			setAutoStart();
+		}
+		else {
+			std::cout << "enjoy app" << std::endl;
+		}
+		answer = "";
+	}
+
+	RegCloseKey(hKey);
 
 	bool slideShow = pathes.size() > 1 ? true : false;
 	short increment = 0;
